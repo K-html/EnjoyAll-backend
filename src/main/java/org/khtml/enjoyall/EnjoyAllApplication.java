7f29f64a328b30e37b53a266c71deacfab2f1782
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2024-08-01 19:45:06 +0900

    Initial commit

---
+++ src/main/java/org/khtml/enjoyall/EnjoyAllApplication.java
@@ -0,0 +1,13 @@
+package org.khtml.enjoyall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/khtml/enjoyall/EnjoyAllApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khtml.enjoyall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
